98e150a6fb37eca70b12f978929a2776ca38110e
huynh <huynhtham3082@gmail.com>
2025-08-10 21:47:59 +0700

    add allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,17 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   projectId: "23foni",
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+     allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 });
+
 
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,3 +16,4 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import '../e2e/tests/TestBase'
+import "allure-cypress";
