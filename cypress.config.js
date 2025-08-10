99ea05a63bda87ad6e1dad8133d76c8abc889638
huynh <huynhtham3082@gmail.com>
2025-08-10 21:01:31 +0700

    fix path

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "23foni",
   e2e: {
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/tests/LoginTest.cy.js
@@ -0,0 +1,6 @@
+// cypress/e2e/example.cy.js
+describe('My First Test', () => {
+  it('Visits Google', () => {
+    cy.visit('https://google.com')
+  })
+})
